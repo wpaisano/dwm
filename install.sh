7df2c2cfed5816c422820c27139ad44196b50818
Walace Paisano <58262296+wpaisano@users.noreply.github.com>
2024-11-22 09:11:21 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-# Atualizar repositórios
-echo "Atualizando repositórios..."
-sudo xbps-install -Syu
+# Variável com o link do repositório
+REPO_URL="https://github.com/wpaisano/dwm"
+REPO_DIR="dwm"
 
-# Instalar os pacotes listados no arquivo packages.txt
-echo "Instalando pacotes do packages.txt..."
+# Atualizando o sistema base
+echo "Atualizando o sistema..."
+sudo xbps-install -Syu -y
+
+# Instalando o Git, caso não esteja instalado
+echo "Verificando instalação do Git..."
+sudo xbps-install -y git
+
+# Clonando o repositório, caso ainda não esteja presente
+if [ ! -d "$REPO_DIR" ]; then
+    echo "Clonando o repositório..."
+    git clone "$REPO_URL"
+else
+    echo "Repositório já existe, atualizando..."
+    cd "$REPO_DIR"
+    git pull
+    cd ..
+fi
+
+# Acessando o diretório do repositório
+cd "$REPO_DIR" || exit
+
+# Instalando pacotes do arquivo packages.txt
+echo "Instalando pacotes listados no arquivo packages.txt..."
 xargs -a packages.txt sudo xbps-install -y
 
 echo "Instalação concluída!"
+
